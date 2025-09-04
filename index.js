df479b8c46b6741b21c92035ea62f109e7fa710b
Amanda <amandauppgard@gmail.com>
2025-09-04 20:57:08 +0300

    actually use dist

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const morgan = require("morgan")
 const app = express()
 app.use(express.json())
+app.use(express.static('dist'))
 
 morgan.token('data', function(req, res) {return JSON.stringify(req.body)})
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
